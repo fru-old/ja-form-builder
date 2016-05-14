2853c6b047ad60a4bb3b031cc7b880d97b1f73c2
Florian Rüberg <florian.rueberg@gmail.com>
2016-05-14 12:36:41 +0200

    Initial commit

---
+++ tools/server.conf.js
@@ -0,0 +1,11 @@
+module.exports = function init(app) {
+
+  app.get('/hello', function (req, res) {
+    res.json({
+      a: 'Hello World',
+      b: req.query.test
+    });
+  });
+
+  return app;
+};
