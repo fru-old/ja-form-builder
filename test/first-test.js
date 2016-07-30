ea7ba045a208572a662f797dfbd1a015e8286b8e
Florian Rüberg <florian.rueberg@gmail.com>
2016-07-31 01:11:14 +0200

    Latest parser

--- test/first-test.js
+++ test/first-test.js
@@ -16,19 +16,9 @@ describe("Parser", function () {
     expect(parse('$parent.test.test')).to.deep.equal([
       { identifier: ['test', 'test'], root: '$parents', index: 0 }
     ]);
+
     expect(parse('$parents[3].test.test')).to.deep.equal([
       { identifier: ['test', 'test'], root: '$parents', index: 3 }
-    ]);
-  });
-
-  it("Should recognize function calls and constants", function () {
-    expect(parse('test.test(test.test, "test", 123, true)')).to.deep.equal([
-      { call: ['test', 'test'], args: [
-        { identifier: ['test', 'test'], root: '$data' },
-        { constant: '"test"' },
-        { constant: '123' },
-        { constant: 'true' }
-      ]}
     ]);
   });
 
@@ -38,19 +28,40 @@ describe("Parser", function () {
       { space: true },
       { operator: '!'},
       { operator: '!'},
-      { constant: true }
+      { constant: 'true' }
     ]);
   });
 
   it("Should parse sub expressions", function () {
-    expect(parse('test[test.test()]')).to.deep.equal([
-      { identifier: 'test'},
-      { expression: [
-        { call: ['test', 'test'], args: [] }
+
+    var subExpression = [
+      { identifier: ['test', 'test'], root: '$data' }
+    ];
+    expect(parse('test.test')).to.deep.equal(subExpression);
+    console.log(JSON.stringify(parse('test[test.test](test())'), null, 4));
+
+    /*expect(parse('test[test.test]')).to.deep.equal([
+      { identifier: [
+        'test',
+        [{ expression: subExpression }]
       ]}
-    ]);
+    ]);*/
   });
-  
+
+  /*
+  console.log(JSON.stringify(parse('test("test" , 124, true)')));
+    it("Should recognize function calls and constants", function () {
+      expect(parse('test.test(test.test, "test", 123, true)')).to.deep.equal([
+        { call: ['test', 'test'], args: [
+          { identifier: ['test', 'test'], root: '$data' },
+          { string: '"test"' },
+          { number: '123' },
+          { constant: 'true' }
+        ]}
+      ]);
+    });
+    */
+
 	/*var result = require('inject!../lib/tested.js')({
 		'./App.js': {}
 	});*/
